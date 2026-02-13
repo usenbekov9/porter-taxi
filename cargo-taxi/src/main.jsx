c33af002d6b95ba270e5e96f990ab1c7e93b3896
Adis <usenbekovadis308@gmail.com>
2026-02-13 17:54:38 +0600

    done77%

--- cargo-taxi/src/main.jsx
+++ cargo-taxi/src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
